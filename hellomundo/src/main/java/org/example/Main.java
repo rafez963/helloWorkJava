7ce4291d4cfbf8328f6d9f22e4eeac97850557a0
rafez963 <72481637+rafez963@users.noreply.github.com>
2024-07-02 21:18:31 -0500

    Add files via upload

---
+++ hellomundo/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        //Definimos la variable nombre
+        String name = "Rafael Meza";
+        //Lo que mostraremos en consola
+        System.out.println("Hola "+name+", Hola mundo. ");
+    }
+}
